61076e0b1e269794737d2bdadb0efd29fdedec25
TALARI LOKESH KUMAR <talarilokesh1406@gmail.com>
2026-03-03 08:50:59 +0000

    first change commit

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function showMessage() {
-    document.getElementById("output").innerText =
-        "Vercel Deployment Successful ✅";
+async function processImage() {
+
+    const file = document.getElementById("imageInput").files[0];
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    const response = await fetch("https://YOUR-BACKEND.onrender.com/detect", {
+        method: "POST",
+        body: formData
+    });
+
+    const data = await response.json();
+
+    document.getElementById("classificationResult").innerText =
+        "Quality: " + data.result;
 }
